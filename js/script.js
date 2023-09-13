7e59f9ff0b008393b411211fdbf00f983eba09a2
kaazisaheb <kharkesh816@gmail.com>
2023-09-13 15:34:24 +0545

    portfolio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top =
+    })
+
+
+
+    // sticky header
+    let header =document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY >100)
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+
+    // animation footer on scroll
+
+}
